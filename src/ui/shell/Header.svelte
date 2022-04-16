d6975b83509ae7cd95ab5f9ffa1fd5fb18bce808
Ivan Khanevich <mail@iamkhan.tech>
2022-04-17 00:40:49 +0300

    chore: update header

--- src/ui/shell/Header.svelte
+++ src/ui/shell/Header.svelte
@@ -1,11 +1,4 @@
 <header class="py-1">
-	<h1 class="logo">Weavo</h1>
+	<h3>Weavo</h3>
 </header>
 
-<style lang="postcss">
-.logo {
-	font-size: var(--font-size-fluid-1);
-	font-weight: var(--font-weight-9);
-	line-height: 1;
-}
-</style>
